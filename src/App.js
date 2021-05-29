88c5d3ac36bdde010c5c4caef5d058bb797a17da
EMILY SOTO <sotoemily03@gmail.com>
2021-05-28 20:10:32 -0400

    Assignment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import DemoPage from './DemoPage'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+		<DemoPage />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/DemoPage.js
@@ -0,0 +1,8 @@
+const DemoPage = () => (
+	<div>
+		<h1>Hello React</h1>
+		<p>This is a sub-component</p>
+	</div>
+)
+
+export default DemoPage;
